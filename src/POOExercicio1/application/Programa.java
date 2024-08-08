8b01fd3b23da0ca738ccc05e0441724b8598320d
vinisanths <vinipsantos0@gmail.com>
2024-08-08 08:27:01 -0300

    Adição dos Exercicios de Orientação a Objetos

---
+++ src/POOExercicio1/application/Programa.java
@@ -0,0 +1,31 @@
+package POOExercicio1.application;
+
+
+import POOExercicio1.entidades.Retangulo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Retangulo a = new Retangulo();
+
+        System.out.println("Digite a ALtura e Largura do retangulo: ");
+        a.altura = sc.nextDouble();
+        a.largura = sc.nextDouble();
+
+        a.area();
+        a.perimetro();
+
+        System.out.printf("Area = %.2f%n", a.area());
+        System.out.printf("Perimetro = %.2f%n", a.perimetro());
+        System.out.printf("Diagonal = %.2f%n", a.diagonal());
+
+        sc.close();
+
+    }
+}
---
+++ src/POOExercicio1/entidades/Retangulo.java
@@ -0,0 +1,16 @@
+package POOExercicio1.entidades;
+
+public class Retangulo {
+    public double altura;
+    public double largura;
+
+    public double area() {
+        return altura * largura;
+    }
+    public double perimetro() {
+        return (altura * 2) + (largura * 2);
+    }
+    public double diagonal() {
+        return Math.sqrt((altura * altura) + (largura * largura));
+    }
+}
