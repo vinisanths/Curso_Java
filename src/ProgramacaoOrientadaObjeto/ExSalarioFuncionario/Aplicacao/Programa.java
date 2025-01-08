56988035a2aa9d5be13371a0b06b7d39afd0f561
vinisanths <vinipsantos0@gmail.com>
2025-01-08 20:50:26 -0300

    Adição de Exercicios

--- src/ProgramacaoOrientadaObjeto/ExSalarioFuncionario/Aplicacao/Programa.java
+++ src/ProgramacaoOrientadaObjeto/ExSalarioFuncionario/Aplicacao/Programa.java
@@ -11,8 +11,8 @@ public class Programa {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
         Funcionario funcionario = new Funcionario();
+
 
         System.out.println("Digite as Informações do Funcionario");
         System.out.print("Nome: ");
