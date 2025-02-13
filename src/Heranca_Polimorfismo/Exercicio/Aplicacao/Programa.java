b615b931036fb682a2053c487fbae8b54b53a3ae
vinisanths <vinipsantos0@gmail.com>
2025-02-13 20:11:10 -0300

    Edição dos Exemplos sobre Herança, Override, classes e métodos Final e Polimorfismo

---
+++ src/Heranca_Polimorfismo/Exercicio/Aplicacao/Programa.java
@@ -0,0 +1,53 @@
+package Heranca_Polimorfismo.Exercicio.Aplicacao;
+
+import Heranca_Polimorfismo.Exercicio.Entidades.Funcionario;
+import Heranca_Polimorfismo.Exercicio.Entidades.FuncionarioTerceirizado;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Funcionario> listFunc = new ArrayList<>();
+
+        System.out.print("Quantidade de Funcionários: ");
+        int numFunc = sc.nextInt();
+
+
+        for (int i = 1; i <= numFunc ; i++) {
+            System.out.println("Dados do funcionario " + i + ":");
+            System.out.print("Terceirizado? (S/N): ");
+            char opcao = sc.next().charAt(0);
+            System.out.print("Nome: ");
+            sc.nextLine();
+            String nome = sc.nextLine();
+            System.out.print("Quantidade de Horas: ");
+            int horas = sc.nextInt();
+            System.out.print("Valor por Hora: ");
+            double valorPorHora = sc.nextDouble();
+
+            if (opcao == 'S') {
+                System.out.print("Custo Adicional: ");
+                double custoAdicional = sc.nextDouble();
+                listFunc.add(new FuncionarioTerceirizado(nome,horas, valorPorHora,custoAdicional ));
+            }else {
+                listFunc.add(new Funcionario(nome, horas, valorPorHora));
+            }
+        }
+
+        System.out.println();
+        System.out.println("Pagamnetos: ");
+        for (Funcionario func : listFunc) {
+            System.out.println(func.getNome() + " - R$" + String.format("%.2f", func.pagamento()));
+        }
+
+
+        sc.close();
+    }
+}
---
+++ src/Heranca_Polimorfismo/Exercicio/Entidades/FuncionarioTerceirizado.java
@@ -0,0 +1,27 @@
+package Heranca_Polimorfismo.Exercicio.Entidades;
+
+public class FuncionarioTerceirizado extends Funcionario {
+    private Double custoAdicional;
+
+    public FuncionarioTerceirizado(){
+        super();
+    }
+
+    public FuncionarioTerceirizado(String nome, Integer horas, Double valorPorHora, Double custoAdicional) {
+        super(nome, horas, valorPorHora);
+        this.custoAdicional = custoAdicional;
+    }
+
+    public Double getCustoAdicional() {
+        return custoAdicional;
+    }
+
+    public void setCustoAdicional(Double custoAdicional) {
+        this.custoAdicional = custoAdicional;
+    }
+
+    @Override
+    public Double pagamento() {
+        return super.pagamento() + custoAdicional * 1.1;
+    }
+}
