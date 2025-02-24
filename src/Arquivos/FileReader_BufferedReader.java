70cb2cfe35cfbd1c51f98e1a89b82df448566c13
vinisanths <vinipsantos0@gmail.com>
2025-02-24 15:37:14 -0300

    Exemplo sobre Leitura de Arquivos Otimizado

--- src/Arquivos/FileReader_BufferedReader.java
+++ src/Arquivos/FileReader_BufferedReader.java
@@ -8,31 +8,17 @@ public class FileReader_BufferedReader {
     public static void main(String[] args) {
 
         String path = "D:\\in.txt";
-        BufferedReader br = null;
-        FileReader fr = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
 
-            while (line!= null){
+            while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Erro: " + e.getMessage());
-        }finally {
-            try {
-                if (br != null){
-                    br.close();
-                }if (fr != null){
-                    fr.close();
-                }
-            }catch (IOException e ){
-                e.printStackTrace();
-            }
         }
     }
 }
