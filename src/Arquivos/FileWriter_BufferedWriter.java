6708501b92a358550a9a2ff3ec515a15bbc5802f
vinisanths <vinipsantos0@gmail.com>
2025-02-24 16:15:20 -0300

    Exemplo sobre Escrita de Arquivos

---
+++ src/Arquivos/FileWriter_BufferedWriter.java
@@ -0,0 +1,30 @@
+package Arquivos;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter_BufferedWriter {
+    public static void main(String[] args) {
+
+        String[] lines = new String[]{
+                "Good Morning!",
+                "Good Afternoon",
+                "Good Nigth"
+        };
+
+        String path = "D:\\teste.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+            System.out.println("Linhas Adicionadas Com Sucesso!");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
