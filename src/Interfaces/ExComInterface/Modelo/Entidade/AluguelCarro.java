df9eb57dfc1de8a0b590b2d6395928ecb5b1c12a
vinisanths <vinipsantos0@gmail.com>
2025-03-04 14:16:25 -0300

    Reapply "Adição do Exemplo sobre Interface"
    This reverts commit 7fcc60ebd612b39f42e447dc334f3343bd5f7930.

---
+++ src/Interfaces/ExComInterface/Modelo/Entidade/AluguelCarro.java
@@ -0,0 +1,53 @@
+package Interfaces.ExComInterface.Modelo.Entidade;
+
+import java.time.LocalDateTime;
+
+public class AluguelCarro {
+
+    private LocalDateTime inicio;
+    private LocalDateTime fim;
+
+    private Veiculo veiculo;
+    private Fatura fatura;
+
+    private AluguelCarro(){
+    }
+
+    public AluguelCarro(LocalDateTime inicio, LocalDateTime fim, Veiculo veiculo) {
+        this.inicio = inicio;
+        this.fim = fim;
+        this.veiculo = veiculo;
+    }
+
+    public LocalDateTime getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(LocalDateTime inicio) {
+        this.inicio = inicio;
+    }
+
+    public LocalDateTime getFim() {
+        return fim;
+    }
+
+    public void setFim(LocalDateTime fim) {
+        this.fim = fim;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Fatura getFatura() {
+        return fatura;
+    }
+
+    public void setFatura(Fatura fatura) {
+        this.fatura = fatura;
+    }
+}
